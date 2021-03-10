58f1909265e67769b5a45841110f7296f00c4104
Aaron Baideme <avbaideme@me.com>
2021-03-10 23:24:40 +0800

    Minor changes and enhancing outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,14 @@
-output "env-dynamic-url" {
-  value = module.eks.cluster_endpoint
-}
-
 output "kubecfg" {
   value = module.eks.kubeconfig
+}
+output "kubernetes-cluster-certificate-authority-data" {
+  value = module.eks.cluster_certificate_authority_data
+}
+
+output "kubernetes-cluster-id" {
+  value = module.eks.cluster_id
+}
+
+output "kubernetes-cluster-endpoint" {
+  value = module.eks.cluster_endpoint
 }
