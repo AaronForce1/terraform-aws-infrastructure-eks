935037083037d75d4ade47da1153c8615813ad21
Aaron Baideme <aaron.baideme@magneticasia.com>
2021-07-03 15:53:33 +0800

    Bugfix: Namespace resource dupe

--- provisioning/kubernetes/namespaces/gitlab-runner.tf
+++ provisioning/kubernetes/namespaces/gitlab-runner.tf
@@ -1,4 +1,4 @@
-resource "kubernetes_namespace" "hashicorp" {
+resource "kubernetes_namespace" "gitlab_runner" {
   count = var.helm_installations.gitlab_runner ? 1 : 0
   metadata {
     name = "gitlab-runner"
