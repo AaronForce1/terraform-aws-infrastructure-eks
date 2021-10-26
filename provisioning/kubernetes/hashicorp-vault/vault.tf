b3e8929db9079fe95c827a2b4e6c15ca1ee4f79b
ronel <ronel.cartas@kitesystems.com>
2021-10-26 16:26:39 +0800

    2 space down

--- provisioning/kubernetes/hashicorp-vault/vault.tf
+++ provisioning/kubernetes/hashicorp-vault/vault.tf
@@ -44,32 +44,31 @@ locals {
         "secretKey": "AWS_SECRET_ACCESS_KEY"
       }
 ])) : yamlencode([])
-  
   haConfig_KMS = yamlencode({
-    enabled: true,
-    replicas: 2,
-    config: <<EOF
-    ui = "true"
+  enabled: true,
+  replicas: 2,
+  config: <<EOF
+  ui = "true"
 
-    listener "tcp" {
-     tls_disable = 1
-     address = "[::]:8200"
-     cluster_address = "[::]:8201"
-    }
+  listener "tcp" {
+    tls_disable = 1
+    address = "[::]:8200"
+    cluster_address = "[::]:8201"
+  }
 
-    seal "awskms" {
-      region     = "${var.aws_region}"
-      kms_key_id =  "${var.enable_aws_vault_unseal ? aws_kms_key.vault[0].key_id : ""}"
-    }
+  seal "awskms" {
+    region     = "${var.aws_region}"
+    kms_key_id =  "${var.enable_aws_vault_unseal ? aws_kms_key.vault[0].key_id : ""}"
+  }
 
-    storage "consul" {
-      path = "vault"
-      address = "HOST_IP:8500"
-    }
- 
-    service_registration "kubernetes" {}
+  storage "consul" {
+    path = "vault"
+    address = "HOST_IP:8500"
+  }
+
+  service_registration "kubernetes" {}
   EOF
-  })  
+  })
 
   haConfig_default = indent(1, yamlencode({
   enabled: true,
