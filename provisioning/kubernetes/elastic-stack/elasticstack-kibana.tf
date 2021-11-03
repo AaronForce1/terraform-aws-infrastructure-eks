a888824aa63f721f5d0ec282770cff45c78fcd59
ronel <ronel.cartas@kitesystems.com>
2021-11-03 11:52:30 +0800

    elastic-stack kibana ingress fix

--- provisioning/kubernetes/elastic-stack/elasticstack-kibana.tf
+++ provisioning/kubernetes/elastic-stack/elasticstack-kibana.tf
@@ -10,7 +10,7 @@ imagePullPolicy: Always
 ingress:
   enabled: true
   annotations:
-    kubernetes.io/ingress-class: nginx
+    kubernetes.io/ingress.class: nginx
     cert-manager.io/cluster-issuer: letsencrypt-prod
     nginx.ingress.kubernetes.io/auth-signin: "https://kibana.${var.app_namespace}-${var.tfenv}.${var.root_domain_name}/oauth2/start?rd=$escaped_request_uri"
     nginx.ingress.kubernetes.io/auth-url: "https://kibana.${var.app_namespace}-${var.tfenv}.${var.root_domain_name}/oauth2/auth"
