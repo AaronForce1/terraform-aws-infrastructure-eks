61de69424ee98cd8446f6de7c73acfbf3e9b6a87
Diederik Damen <diederik.damen@magneticasia.com>
2021-11-08 22:21:07 +0800

    st1 default to retain

--- provisioning/kubernetes/aws-support/storage-class.st1.tf
+++ provisioning/kubernetes/aws-support/storage-class.st1.tf
@@ -3,6 +3,7 @@ resource "kubernetes_storage_class" "st1-storage-class" {
     name = "st1"
   }
   storage_provisioner = "kubernetes.io/aws-ebs"
+  reclaim_policy      = "Retain"
   parameters = {
     type = "st1"
   }
