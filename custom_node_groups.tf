2ba0e6cade009c60945b925a207258b5a32715fb
ronel <ronel.cartas@kitesystems.com>
2021-11-11 17:46:30 +0800

    enable back depends_on

--- custom_node_groups.tf
+++ custom_node_groups.tf
@@ -33,7 +33,7 @@ resource "aws_eks_node_group" "custom_node_group" {
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-  #depends_on = [
-  #  module.eks-vpc, module.eks
-  #]
+  depends_on = [
+    module.eks-vpc, module.eks
+  ]
 }
