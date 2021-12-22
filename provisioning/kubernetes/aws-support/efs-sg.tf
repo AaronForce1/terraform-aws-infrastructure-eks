a4d02b5018b74afa295660ea5554927966233d00
clayton <clayton.stevenson@magneticasia.com>
2021-12-22 14:14:26 +0100

    minor syntax correction

--- provisioning/kubernetes/aws-support/efs-sg.tf
+++ provisioning/kubernetes/aws-support/efs-sg.tf
@@ -1,13 +1,13 @@
 resource "aws_security_group" "efs_security_group" {
   name        = "${var.app_name}-${var.app_namespace}-${var.tfenv}-efs"
   description = "${var.app_name}-${var.app_namespace}-${var.tfenv}-efs"
-  vpc_id      = module.eks-vpc.vpc_id
+  vpc_id      = "${module.eks-vpc.vpc_id}"
   ingress {
     description      = "NFS from VPC"
     from_port        = 2049
     to_port          = 2049
     protocol         = "tcp"
-    cidr_blocks      = [module.subnet_addrs.base_cidr_block]
+    cidr_blocks      = ["${module.subnet_addrs.base_cidr_block}"]
   }
 
   tags = {
