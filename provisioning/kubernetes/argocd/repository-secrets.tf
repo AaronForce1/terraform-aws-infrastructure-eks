625f3ac770a01e8801ddaf8a0b8e5546c97432c2
marcus.cheng <marcus.cheng@hextrust.com>
2022-05-19 14:59:29 +0800

    provide ArgoCD generic-helm-chart deploy token

--- provisioning/kubernetes/argocd/repository-secrets.tf
+++ provisioning/kubernetes/argocd/repository-secrets.tf
@@ -1,10 +1,22 @@
-data "aws_ssm_parameter" "argocd_ssh" {
-  name = "/repository/ssh"
+data "aws_ssm_parameter" "argocd_infrastructure_ssh" {
+  name = "/repository/kubernetes-infrastructure/ssh"
 }
 
-resource "kubernetes_secret" "argocd_ssh" {
+data "aws_ssm_parameter" "argocd_application_ssh" {
+  name = "/repository/kubernetes-application/ssh"
+}
+
+data "aws_ssm_parameter" "argocd_generic_helm_chart_registry_username" {
+  name = "/repository/generic-helm-chart/username"
+}
+
+data "aws_ssm_parameter" "argocd_generic_helm_chart_registry_password" {
+  name = "/repository/generic-helm-chart/password"
+}
+
+resource "kubernetes_secret" "argocd_infrastructure_ssh" {
   metadata {
-    name      = "repository-ssh-key"
+    name      = "repository-infrastructure-ssh-key"
     namespace = "argocd"
     labels = {
       "argocd.argoproj.io/secret-type" = "repo-creds"
@@ -12,6 +24,38 @@ resource "kubernetes_secret" "argocd_ssh" {
   }
 
   data = {
-    sshPrivateKey = data.aws_ssm_parameter.argocd_ssh.value
+    sshPrivateKey = data.aws_ssm_parameter.argocd_infrastructure_ssh.value
+  }
+}
+
+resource "kubernetes_secret" "argocd_application_ssh" {
+  metadata {
+    name      = "repository-application-ssh-key"
+    namespace = "argocd"
+    labels = {
+      "argocd.argoproj.io/secret-type" = "repo-creds"
+    }
+  }
+
+  data = {
+    sshPrivateKey = data.aws_ssm_parameter.argocd_application_ssh.value
+  }
+}
+
+resource "kubernetes_secret" "argocd_helm_chart_registry" {
+  metadata {
+    name      = "repository-generic-helm-chart"
+    namespace = "argocd"
+    labels = {
+      "argocd.argoproj.io/secret-type" = "repository"
+    }
+  }
+
+  data = {
+    name     = "generic-helm-chart"
+    url      = "https://gitlab.int.hextech.io/api/v4/projects/645/packages/helm/alpha"
+    type     = "helm"
+    username = data.aws_ssm_parameter.argocd_generic_helm_chart_registry_username.value
+    password = data.aws_ssm_parameter.argocd_generic_helm_chart_registry_password.value
   }
 }
