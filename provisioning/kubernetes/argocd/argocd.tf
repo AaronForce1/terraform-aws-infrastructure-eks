eaf474e03c66d6cc8ef3a3db35a741d7762d3936
Aaron Baideme <aaron.baideme@hextrust.com>
2022-06-09 13:47:40 +0800

    Minor updates

--- provisioning/kubernetes/argocd/argocd.tf
+++ provisioning/kubernetes/argocd/argocd.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "argocd" {
-  name             = "argocd"
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argo-cd"
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
   # chart            = "../../../../../SYSTEM/CLUSTERS/infra/develop/argo-cd"
   namespace        = "argocd"
   create_namespace = false
