80168dbd30c8de7364c2c24383b987051e0ba6fc
Aditya Gupta <aditya.gupta@hextrust.com>
2023-01-20 13:11:28 +0800

    fix: variables-in-teleport

---
+++ provisioning/kubernetes/teleport/variables.tf
@@ -0,0 +1,15 @@
+variable "chart_version" {
+  type    = string
+}
+
+variable "cluster_name" {
+  type    = string
+}
+
+variable "proxy_address" {
+  type = string
+}
+
+variable "auth_token" {
+  type = string
+}
