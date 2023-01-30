f9cfb083b0fce0b6759ef2f3f3f49eab9684ce7f
vienne.wan <vienne.wan@hextrust.com>
2023-01-30 16:55:11 +0800

    feat: change to use for_each instead count

--- provisioning/kubernetes/argocd/repository-secrets.tf
+++ provisioning/kubernetes/argocd/repository-secrets.tf
@@ -1,23 +1,3 @@
-# resource "kubernetes_secret" "argocd_application_repository_secrets" {
-#   count = length(var.repository_secrets)
-
-#   metadata {
-#     name      = "repository-${var.repository_secrets[count.index].name}"
-#     namespace = "hexsafe-alpha" # just for testing
-#     labels = {
-#       "argocd.argoproj.io/secret-type" = "repository"
-#     }
-#   }
-
-#   data = {
-#     name     = var.repository_secrets[count.index].name
-#     url      = var.repository_secrets[count.index].url
-#     type     = var.repository_secrets[count.index].type
-#     username = var.repository_secrets[count.index].secrets_store != "ssm" ? var.repository_secrets[count.index].username : data.aws_ssm_parameter.infrastructure_credentials_username[var.repository_secrets[count.index].username].value
-#     password = var.repository_secrets[count.index].secrets_store != "ssm" ? var.repository_secrets[count.index].password : data.aws_ssm_parameter.infrastructure_credentials_password[var.repository_secrets[count.index].password].value
-#   }
-# }
-
 resource "kubernetes_secret" "argocd_application_repository_secrets" {
   for_each = {
     for repository_secret in var.repository_secrets : repository_secret.name => repository_secret
@@ -25,7 +5,7 @@ resource "kubernetes_secret" "argocd_application_repository_secrets" {
 
   metadata {
     name      = "repository-${each.value.name}"
-    namespace = "hexsafe-alpha" # just for testing
+    namespace = "argocd"
     labels = {
       "argocd.argoproj.io/secret-type" = "repository"
     }
