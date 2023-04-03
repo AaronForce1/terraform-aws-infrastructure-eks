5d138829e00c7f2a6f323fb299b409edd27d4566
ryan <phuc.truong@hextrust.com>
2023-04-03 21:43:08 +0700

    feat: add labels and annotations for namespace

--- kubernetes-namespaces.tf
+++ kubernetes-namespaces.tf
@@ -1,14 +1,19 @@
 resource "kubernetes_namespace" "cluster" {
   depends_on = [module.eks]
-  for_each   = toset(local.namespaces)
-
+  for_each = {
+    for namespace in var.custom_namespaces : namespace.name => namespace
+  }
   metadata {
-    labels = {
-      name              = each.key
-      "Terraform"       = true
-      "eks/name"        = local.name_prefix
-      "eks/environment" = var.tfenv
-    }
-    name = each.key
+    labels = merge(
+      {
+        name              = each.value.name
+        "Terraform"       = true
+        "eks/name"        = local.name_prefix
+        "eks/environment" = var.tfenv
+      },
+      try(each.value.labels, [])
+    )
+    annotations = try(each.value.annotations, [])
+    name = each.value.name
   }
 }
