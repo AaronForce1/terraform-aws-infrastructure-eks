717aac10b3f9de29e76b8c7cf03ee34d954c1016
Aaron Baideme <aaron.baideme@hextrust.com>
2023-05-31 14:31:20 +0800

    fix: Minor fixes to variable logic for optional components

--- variables.tf
+++ variables.tf
@@ -456,11 +456,11 @@ variable "helm_configurations" {
       }))
     }))
     teleport = optional(object({
-      installations = list(object({
+      installations = optional(list(object({
         chart_name    = string
         chart_version = string
         values_file   = string
-      }))
+      })))
     }))
   })
   default = {
