7d274cec7bae88caa9232b08829a950b447df644
Suraj Tiwari <suraj.tiwari@hextrust.com>
2023-06-14 14:27:24 +0800

    feat: add parameter store terraform module

---
+++ aws-parameter-store/main.tf
@@ -0,0 +1,19 @@
+module "ssm-parameter" {
+  source  = "terraform-aws-modules/ssm-parameter/aws"
+  version = "1.0.1"
+  for_each = var.parameters
+
+  name            = try(each.value.name, each.key)
+  value           = try(each.value.value, null)
+  values          = try(each.value.values, [])
+  type            = try(each.value.type, null)
+  secure_type     = try(each.value.secure_type, true)
+  description     = try(each.value.description, null)
+  tier            = try(each.value.tier, "Standard")
+  overwrite       = try(each.value.overwrite, null)
+  key_id          = try(each.value.key_id, null)
+  allowed_pattern = try(each.value.allowed_pattern, null)
+  data_type       = try(each.value.data_type, null)
+
+  tags = try(each.value.tags, {})
+}
