bc2d4899bcd57cb1a9ec46f679642f42631613ee
Suraj Tiwari <suraj.tiwari@hextrust.com>
2023-07-06 14:41:10 +0700

    feat: change irsa and role name

--- karpenter/main.tf
+++ karpenter/main.tf
@@ -6,8 +6,8 @@ module "karpenter" {
   version = "19.15.3"
 
   cluster_name                    = var.karpenter.cluster_name #module.eks.cluster_name
-  irsa_name                       = "karpenter-irsa"
-  iam_role_name                   = "karpenter-role"
+  irsa_name                       = "karpenter-irsa-${var.karpenter.cluster_name}"
+  iam_role_name                   = "karpenter-role-${var.karpenter.cluster_name}"
   irsa_use_name_prefix            = false
   iam_role_use_name_prefix        = false
   irsa_oidc_provider_arn          = var.karpenter.irsa_oidc_provider_arn # module.eks.oidc_provider_arn
