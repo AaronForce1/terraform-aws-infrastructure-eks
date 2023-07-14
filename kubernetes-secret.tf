293b13ca07450c6ec5836a50f58cefbe01ec03f3
Suraj Tiwari <suraj.tiwari@hextrust.com>
2023-07-14 15:20:56 +0700

    feat: modify secret resource

--- kubernetes-secret.tf
+++ kubernetes-secret.tf
@@ -29,7 +29,8 @@ resource "kubernetes_secret" "kubernetes_secret" {
       try(each.value.labels, {})
     )
   }
-  data = each.value.secrets_store != "ssm" ? (try(yamldecode(each.value.data), { (each.value.name) = each.value.data })) : (try(jsondecode(data.aws_ssm_parameter.kubernetes_secret["${each.value.name}-${each.value.namespace}"].value), { (each.value.name) = data.aws_ssm_parameter.kubernetes_secret["${each.value.name}-${each.value.namespace}"].value }))type = coalesce(each.value.type, "Opaque")
+  data = each.value.secrets_store != "ssm" ? (try(yamldecode(each.value.data), { (each.value.name) = each.value.data })) : (try(jsondecode(data.aws_ssm_parameter.kubernetes_secret["${each.value.name}-${each.value.namespace}"].value), { (each.value.name) = data.aws_ssm_parameter.kubernetes_secret["${each.value.name}-${each.value.namespace}"].value }))
+  type = coalesce(each.value.type, "Opaque")
 }
 
 ###########################################################################
