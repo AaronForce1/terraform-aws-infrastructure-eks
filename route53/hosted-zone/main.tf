04c5c56661a2a67dbefbbc33524ab40a0d4b8420
Suraj Tiwari <suraj.tiwari@hextrust.com>
2023-07-17 13:16:24 +0700

    feat: add route53

---
+++ route53/hosted-zone/main.tf
@@ -0,0 +1,15 @@
+variable "zones" {
+  type = any
+}
+module "route53_zones" {
+  source  = "terraform-aws-modules/route53/aws//modules/zones"
+  version = "2.10.2"
+
+  zones = var.zones
+
+}
+
+output "route53_zone_zone_id" {
+  description = "Zone ID of Route53 zone"
+  value       = module.route53_zones.route53_zone_zone_id
+}
