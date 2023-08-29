5fb6f30b4a6dea8947bfed83b54768eabc6c02eb
ryan <phuc.truong@hextrust.com>
2023-08-29 21:03:51 +0700

    chore: fix hashicorp/aws provider

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = "~> 4.5"
+      version               = "~> 5.0"
       configuration_aliases = [aws.secondary]
     }
     gitlab = {
